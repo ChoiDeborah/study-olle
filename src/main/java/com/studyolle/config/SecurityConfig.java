5dfa222aec728aa5ed4ac57d8a731363c63c8e73
ChoiDeborah <adorablejimini@gmail.com>
2021-01-15 21:57:21 +0900

    회원 가입 뷰

--- src/main/java/com/studyolle/config/SecurityConfig.java
+++ src/main/java/com/studyolle/config/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.studyolle.config;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -18,5 +20,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/email-login", "/check-email-login", "/login-link").permitAll()
                 .mvcMatchers(HttpMethod.GET, "/profile/*").permitAll()
                 .anyRequest().authenticated();
+    }
+
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        // static한 리소스들은 security filter 적용하지 않도록
+        web.ignoring()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 }

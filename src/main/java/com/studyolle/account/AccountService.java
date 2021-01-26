67486bdeafd9f1895220a83cb7ad610557a88396
ChoiDeborah <adorablejimini@gmail.com>
2021-01-26 17:17:22 +0900

    회원 가입 인증 메일 확인

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -6,6 +6,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -15,6 +16,7 @@ public class AccountService {
     private final JavaMailSender javaMailSender;
     private final PasswordEncoder passwordEncoder;
 
+    @Transactional  // 영속 유지
     public void processNewAccount(SignUpForm signUpForm) {
         Account newAccount = saveNewAccout(signUpForm);
         newAccount.generateEmailCheckToken();
@@ -38,8 +40,8 @@ public class AccountService {
     private void sendSignUpConfirmEmail(Account newAccount) {
         SimpleMailMessage mailMessage = new SimpleMailMessage();
         mailMessage.setSubject("스터디 올래 회원 가입 인증");
-        mailMessage.setText("check-email-token?token=" + newAccount.getEmailCheckToken()
-                + "&email" + newAccount.getEmail()); // 본문
+        mailMessage.setText("/check-email-token?token=" + newAccount.getEmailCheckToken()
+                + "&email=" + newAccount.getEmail()); // 본문
 
         javaMailSender.send(mailMessage);
     }

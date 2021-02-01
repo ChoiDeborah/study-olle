8ebba98564177315fabf294bc73f3064a2b47b45
ChoiDeborah <adorablejimini@gmail.com>
2021-02-01 16:07:09 +0900

    패스워드 수정

--- src/main/java/com/studyolle/account/AccountService.java
+++ src/main/java/com/studyolle/account/AccountService.java
@@ -99,4 +99,9 @@ public class AccountService implements UserDetailsService {
 
         // TODO 문제가 하나 더 남았습니다.
     }
+
+    public void updatePassword(Account account, String newPassword) {
+        account.setPassword(passwordEncoder.encode(newPassword));
+        accountRepository.save(account);    // merge
+    }
 }

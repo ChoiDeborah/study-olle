0fdc0663d440d52c61ef0e171ad69e616441223b
ChoiDeborah <adorablejimini@gmail.com>
2021-02-07 15:23:42 +0900

    스터디 도메인

---
+++ src/main/java/com/studyolle/domain/Study.java
@@ -0,0 +1,58 @@
+package com.studyolle.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.Set;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Study {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToMany
+    private Set<Account> managers;
+
+    @ManyToMany
+    private Set<Account> members;
+
+    @Column(unique = true)
+    private String path;
+
+    private String title;
+
+    private String shortDescription;
+
+    // 명시적으로 해놓은것 Lob은 기본값 EAGER임
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String fullDescription;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String image;
+
+    @ManyToMany
+    Set<Tag> tags;
+
+    @ManyToMany
+    Set<Zone> zones;
+
+    private LocalDateTime publishedDateTime;
+
+    private LocalDateTime closedDateTime;
+
+    private LocalDateTime recruitingUpdatedDateTime;
+
+    private boolean recruiting;
+
+    private boolean published;
+
+    private boolean closed;
+
+    private boolean useBanner;
+
+}

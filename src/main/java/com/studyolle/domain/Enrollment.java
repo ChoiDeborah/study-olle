05f7903f7b61f41651771a407559ceca7bf93cfd
ChoiDeborah <adorablejimini@gmail.com>
2021-02-10 18:59:31 +0900

    모임 도메인

---
+++ src/main/java/com/studyolle/domain/Enrollment.java
@@ -0,0 +1,32 @@
+package com.studyolle.domain;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter @EqualsAndHashCode(of = "id")
+public class Enrollment {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @ManyToOne  // 다가 되는 쪽에서 관계의 매핑을 Event의 FK로 가지고 있는걸 젤 많이 씀.
+    private Event event;
+
+    @ManyToOne
+    private Account account;
+
+    private LocalDateTime enrolledAt; // 선착순이니 순서 정렬 기준이 될 것임
+
+    private boolean accepted;
+
+    private boolean attended;
+
+}

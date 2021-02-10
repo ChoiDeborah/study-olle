349a2cca2d643385030685a7816d49bebb2db4b2
ChoiDeborah <adorablejimini@gmail.com>
2021-02-10 22:07:28 +0900

    모임 만들기 폼 서브밋

---
+++ src/main/java/com/studyolle/event/validator/EventValidator.java
@@ -0,0 +1,42 @@
+package com.studyolle.event.validator;
+
+import com.studyolle.event.form.EventForm;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.time.LocalDateTime;
+
+@Component
+public class EventValidator implements Validator {
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return EventForm.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        EventForm eventForm = (EventForm) target;
+
+        if (eventForm.getEndEnrollmentDateTime().isBefore(LocalDateTime.now())) {
+            errors.rejectValue("endEnrollmentDateTime", "wrong.datetime", "모임 접수 종료 일시를 정확히 입력하세요.");
+        }
+
+        if (isNotValidEndDateTime(eventForm)) {
+            errors.rejectValue("endDateTime", "wrong.datetime", "모임 접수 종료 일시를 정확히 입력하세요.");
+        }
+
+        if (isNotValidStartDateTime(eventForm)) {
+
+        }
+    }
+
+    private boolean isNotValidStartDateTime(EventForm eventForm) {
+        return eventForm.getStartDateTime().isBefore(eventForm.getEndEnrollmentDateTime());
+    }
+
+    private boolean isNotValidEndDateTime(EventForm eventForm) {
+        LocalDateTime endDateTime = eventForm.getEndDateTime();
+        return endDateTime.isBefore(eventForm.getStartDateTime()) || endDateTime.isBefore(eventForm.getEndEnrollmentDateTime());
+    }
+}

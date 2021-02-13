7d67f5c68f2fd13e7e8b622b4d6de33bbb0f7b5b
ChoiDeborah <adorablejimini@gmail.com>
2021-02-13 14:19:41 +0900

    모임 수정

--- src/main/java/com/studyolle/event/validator/EventValidator.java
+++ src/main/java/com/studyolle/event/validator/EventValidator.java
@@ -1,5 +1,6 @@
 package com.studyolle.event.validator;
 
+import com.studyolle.domain.Event;
 import com.studyolle.event.form.EventForm;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 
 @Component
 public class EventValidator implements Validator {
+
     @Override
     public boolean supports(Class<?> clazz) {
         return EventForm.class.isAssignableFrom(clazz);
@@ -16,14 +18,14 @@ public class EventValidator implements Validator {
 
     @Override
     public void validate(Object target, Errors errors) {
-        EventForm eventForm = (EventForm) target;
+        EventForm eventForm = (EventForm)target;
 
-        if (eventForm.getEndEnrollmentDateTime().isBefore(LocalDateTime.now())) {
+        if (isNotValidEndEnrollmentDateTime(eventForm)) {
             errors.rejectValue("endEnrollmentDateTime", "wrong.datetime", "모임 접수 종료 일시를 정확히 입력하세요.");
         }
 
         if (isNotValidEndDateTime(eventForm)) {
-            errors.rejectValue("endDateTime", "wrong.datetime", "모임 접수 종료 일시를 정확히 입력하세요.");
+            errors.rejectValue("endDateTime", "wrong.datetime", "모임 종료 일시를 정확히 입력하세요.");
         }
 
         if (isNotValidStartDateTime(eventForm)) {
@@ -35,8 +37,18 @@ public class EventValidator implements Validator {
         return eventForm.getStartDateTime().isBefore(eventForm.getEndEnrollmentDateTime());
     }
 
+    private boolean isNotValidEndEnrollmentDateTime(EventForm eventForm) {
+        return eventForm.getEndEnrollmentDateTime().isBefore(LocalDateTime.now());
+    }
+
     private boolean isNotValidEndDateTime(EventForm eventForm) {
         LocalDateTime endDateTime = eventForm.getEndDateTime();
         return endDateTime.isBefore(eventForm.getStartDateTime()) || endDateTime.isBefore(eventForm.getEndEnrollmentDateTime());
+    }
+
+    public void validateUpdateForm(EventForm eventForm, Event event, Errors errors) {
+        if (eventForm.getLimitOfEnrollments() < event.getNumberOfAcceptedEnrollments()) {
+            errors.rejectValue("limitOfEnrollments", "wrong.value", "확인된 참기 신청보다 모집 인원 수가 커야 합니다.");
+        }
     }
 }
